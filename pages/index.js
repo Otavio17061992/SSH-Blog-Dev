22a84b73ea3448a1dc980a456ef86e5cb25467bb
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2024-02-06 01:41:53 +0000

    adicioando pages

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home(){
+    return<h1>Código é a linguagem que dá vida a ideias,
+              transformando linhas aparentemente estáticas em execuções
+              dinâmicas que moldam o futuro digital
+          </h1>
+}
+
+export default Home;
