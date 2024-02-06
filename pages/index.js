b8b2bf34130fbb7e0daea117f6e7e12105ac667d
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2024-02-06 02:51:05 +0000

    alterando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return<h1>Código é a linguagem que dá vida a ideias,
               transformando linhas aparentemente estáticas em execuções
-              dinâmicas que moldam o futuro digital
+              dinâmicas que moldam o futuro digital hhaha
           </h1>
 }
 
